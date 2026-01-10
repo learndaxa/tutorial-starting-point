1050371e07f32e5ee4843703dd3a99cc4a1c7bf7
deccer <decccer@outlook.com>
2026-01-10 17:20:22 +0100

    Use [[maybe_unused]]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main(int argc, char const *argv[])
+int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[])
 {
     std::cout << "Hello World!" << std::endl;
     return 0;
